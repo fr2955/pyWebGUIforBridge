50d47e18456346cb49acd8bf19459363a2997e5c
fr2955 <fr2955@email.vccs.edu>
2018-11-17 14:54:01 -0500

    made a example alsmost working
    Signed-off-by: fr2955 <fr2955@email.vccs.edu>

--- code/app.py
+++ code/app.py
@@ -1,12 +1,22 @@
+import serial
+from flask import request
 from flask import Flask
-from arduinoSerial import *
 app = Flask(__name__)
 
+@app.route('/', methods = ['POST', 'GET'])
+def hello():
+    if request.method == 'POST':
+        print(request.form['submit'])
+        ser = serial.Serial('COM5', 9600, timeout=1)
+        if request.form['submit'] == 'OPEN':
+            print("Open")
+            ser.write('4')
+        elif request.form['submit'] == 'CLOSE':
+            print("Close")
+            ser.write('5')
+        else:
+            pass  # unknown
+    return '<form action="/" method="POST"><input type="submit" name="submit" value="OPEN"><input type="submit" name="submit" value="CLOSE"></form>'
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
