b22fb209c19b9762fc46591893aa4d5f4b37d41c
fr2955 <fr2955@email.vccs.edu>
2018-11-17 18:49:20 -0500

    got it working but I need to fix it
    Signed-off-by: fr2955 <fr2955@email.vccs.edu>

--- code/app.py
+++ code/app.py
@@ -1,22 +1,30 @@
 import serial
 from flask import request
 from flask import Flask
+import struct
+from time import sleep
+
+ser = serial.Serial('COM5', 9600)
+ser.setDTR(True)
+sleep(1)
+ser.flushInput()
+ser.setDTR(False)
 app = Flask(__name__)
 
 @app.route('/', methods = ['POST', 'GET'])
-def hello():
+def webApp():
     if request.method == 'POST':
         print(request.form['submit'])
-        ser = serial.Serial('COM5', 9600, timeout=1)
+
         if request.form['submit'] == 'OPEN':
             print("Open")
-            ser.write('4')
+            ser.write(str.encode('4'))
         elif request.form['submit'] == 'CLOSE':
             print("Close")
-            ser.write('5')
+            ser.write(str.encode('5'))
         else:
             pass  # unknown
     return '<form action="/" method="POST"><input type="submit" name="submit" value="OPEN"><input type="submit" name="submit" value="CLOSE"></form>'
 
 if __name__ == "__main__":
-    app.run()
+    webApp.run()
