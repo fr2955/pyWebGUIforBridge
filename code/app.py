adf660855117195d3e3d6266c3dce3bf4680d454
fr2955 <45108039+fr2955@users.noreply.github.com>
2018-11-17 22:50:18 -0500

    Works, Part one done, second phases add html, css and a status on the web page
    Signed-off-by: fr2955 <fr2955@email.vccs.edu>

--- code/app.py
+++ code/app.py
@@ -1,14 +1,15 @@
 import serial
 from flask import request
 from flask import Flask
-import struct
 from time import sleep
+
 
 ser = serial.Serial('COM5', 9600)
 ser.setDTR(True)
 sleep(1)
 ser.flushInput()
 ser.setDTR(False)
+
 app = Flask(__name__)
 
 @app.route('/', methods = ['POST', 'GET'])
@@ -27,4 +28,4 @@ def webApp():
     return '<form action="/" method="POST"><input type="submit" name="submit" value="OPEN"><input type="submit" name="submit" value="CLOSE"></form>'
 
 if __name__ == "__main__":
-    webApp.run()
+    webApp.run(app)
